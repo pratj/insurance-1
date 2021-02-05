5babeb0d2c1a79a594d3bbcf043a22730b830b8a
Param Attri <attri.param@gmail.com>
2021-02-05 18:16:41 +0530

    shared constants

--- Frontend/InsuranceApplication/src/shared/constants/routes.js
+++ Frontend/InsuranceApplication/src/shared/constants/routes.js
@@ -1,1 +1,20 @@
-export default Object.freeze({});
+export default Object.freeze({
+  HOME: {
+    route: "/",
+  },
+  QUOTE: {
+    route: "/quote",
+  },
+  PARTNERS: {
+    route: "/partners",
+  },
+  PRODUCTS: {
+    route: "/products",
+  },
+  SUGGESTIONS: {
+    route: "/suggestions",
+  },
+  ABOUT: {
+    route: "/aboutus",
+  },
+});
